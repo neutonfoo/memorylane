ec62da2001f826b754323f729cd114f59ad56f5f
Neuton Foo <neuton.foo@gmail.com>
2019-01-11 22:59:09 -0800

    Added .gitignore

--- index.php
+++ index.php
@@ -2,9 +2,14 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <link rel="stylesheet" href="style.css">
     <title></title>
   </head>
   <body>
-
+    <div id="mainContainer">
+      <div class="">
+        
+      </div>
+    </div>
   </body>
 </html>
