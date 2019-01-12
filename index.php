1590a9451a89ba377c9a32b5fa24339f2de8bc2f
J Hill <chirolhi@usc.edu>
2019-01-12 00:22:34 -0800

    added a bunch of stuffs (bootstrap)

--- index.php
+++ index.php
@@ -2,14 +2,26 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="style.css">
+    <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="css/style.css"/>
     <title></title>
   </head>
   <body>
     <div id="mainContainer">
-      <h1>Memory Lane</h1>
-      <div class="asd">
+      <div id="title">Memory Lane</div>
+
+      <!-- choose if create or follow a hunt -->
+      <div id="options">
+        Choose your adventure
+        <button class="" type="button" name="button">Create a Hunt</button>
+        <button class="" type="button" name="button">Follow a Hunt</button>
       </div>
     </div>
+
+    <script type="text/javascript">
+
+    </script>
   </body>
 </html>
