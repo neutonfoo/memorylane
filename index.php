e6786b0070af9f0b1c5d0b08264d8d2902abe17e
J Hill <chirolhi@usc.edu>
2019-01-12 11:41:38 -0800

    Basic formatting changes

--- index.php
+++ index.php
@@ -4,13 +4,14 @@
     <meta charset="utf-8">
     <script type="text/javascript" src="js/jquery-3.3.1.min.js"></script>
     <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Kodchasan|Permanent+Marker|Shadows+Into+Light|Special+Elite" rel="stylesheet">
     <link rel="stylesheet" href="css/bootstrap.min.css"/>
     <link rel="stylesheet" href="css/style.css"/>
     <link rel="icon" type="image/x-icon" href="favicon.ico" />
     <title>Memory Lane | Home</title>
   </head>
   <body>
-    <div id="mainContainer">
+    <div class="mainContainer">
       <div id="header">
         <div id="title">
           Memory Lane
@@ -25,8 +26,8 @@
       <div id="options">
         Choose your adventure
         <div>
-          <a class="btn btn-outline-primary" href="create.php">Create a Hunt</a>
-          <a class="btn btn-outline-primary" href="hunts.php">Follow a Hunt</a>
+          <a class="btn btn-celadon startBtn" href="create.php">Create a Hunt</a>
+          <a class="btn btn-celadon startBtn" href="hunts.php">Follow a Hunt</a>
         </div>
       </div>
     </div>
